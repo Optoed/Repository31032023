0c1f4e4b47cacaa652b8d65ac47d6c92a8b60969
Optoed <origin-optoed@mail.ru>
2023-03-31 20:54:15 +0300

    2. Рекурсивное вычисление n-ого числа Фибоначчи и его вывод. Последовательность чисел начинается с 1

--- file.cpp
+++ file.cpp
@@ -2,7 +2,26 @@
 #include <iostream>
 using namespace std;
 
+unsigned long long int fib(int n)
+{
+    if (n == 0)
+        return 1;
+
+    if (n == 1 || n == 2)
+        return 1;
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+
 int main() {
     cout << "Hello, World!" << endl;
+
+    setlocale(LC_ALL, "RUS");
+    int n;
+    cout << "n = ";
+    cin >> n;
+    unsigned long long int x = fib(n);
+    cout << "n-ый число Фибоначчи = " << x << endl;
 }
 
